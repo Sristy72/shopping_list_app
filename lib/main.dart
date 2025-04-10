6db65c5300ad1eaca5abea37473d8437666e712a
isita <eshitamondol72@gmail.com>
2025-04-10 22:28:58 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Shopping App',
+      theme: ThemeData.dark().copyWith(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Color.fromARGB(255, 147, 229, 250),
+          brightness: Brightness.dark,
+          surface: Color.fromARGB(255, 42, 51, 59)
+        ),
+        scaffoldBackgroundColor: Color.fromARGB(255, 50, 58, 60)
+      ),
+     // home: ,
+    );
+  }
+}
